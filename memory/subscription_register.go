76ecc429841e3fb0392f89c4424b43a26bf77218
Denis Sablic <denis.sablic@gmail.com>
2017-10-19 08:36:04 +0200

    Bring back subscription register

---
+++ memory/subscription_register.go
@@ -0,0 +1,70 @@
+package memory
+
+import (
+	"sync"
+
+	"github.com/roncohen/faye-go/utils"
+)
+
+type SubscriptionRegister struct {
+	clientByPattern  map[string]utils.StringSet
+	patternsByClient map[string]utils.StringSet
+	mutex            sync.RWMutex
+}
+
+func NewSubscriptionRegister() *SubscriptionRegister {
+	return &SubscriptionRegister{
+		clientByPattern:  make(map[string]utils.StringSet),
+		patternsByClient: make(map[string]utils.StringSet),
+	}
+}
+
+func (sr SubscriptionRegister) AddSubscription(clientId string, patterns []string) {
+	sr.mutex.Lock()
+	defer sr.mutex.Unlock()
+	for _, pattern := range patterns {
+		_, ok := sr.clientByPattern[pattern]
+		if !ok {
+			sr.clientByPattern[pattern] = utils.NewStringSet()
+		}
+		sr.clientByPattern[pattern].Add(clientId)
+	}
+
+	_, ok := sr.patternsByClient[clientId]
+	if !ok {
+		sr.patternsByClient[clientId] = utils.NewStringSet()
+	}
+	sr.patternsByClient[clientId].AddMany(patterns)
+}
+
+func (sr SubscriptionRegister) RemoveSubscription(clientId string, patterns []string) {
+	sr.mutex.Lock()
+	defer sr.mutex.Unlock()
+
+	for _, pattern := range patterns {
+		sr.clientByPattern[pattern].Remove(clientId)
+		sr.patternsByClient[clientId].Remove(pattern)
+	}
+
+}
+
+func (sr SubscriptionRegister) GetClients(patterns []string) []string {
+	StringSet := utils.NewStringSet()
+	sr.mutex.RLock()
+	defer sr.mutex.RUnlock()
+
+	for _, pattern := range patterns {
+		StringSet.AddMany(sr.clientByPattern[pattern].GetAll())
+	}
+	return StringSet.GetAll()
+}
+
+func (sr SubscriptionRegister) RemoveClient(clientId string) {
+	sr.mutex.Lock()
+	defer sr.mutex.Unlock()
+
+	for _, pattern := range sr.patternsByClient[clientId].GetAll() {
+		sr.clientByPattern[pattern].Remove(clientId)
+	}
+	delete(sr.patternsByClient, clientId)
+}
