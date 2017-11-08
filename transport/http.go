0a0f3dd7ba67163a2fe963b84adbc0b696289bd4
Denis Sablic <denis.sablic@gmail.com>
2017-11-08 08:51:05 +0100

    Ensure no long polling channel is left behind

--- transport/http.go
+++ transport/http.go
@@ -2,6 +2,7 @@ package transport
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -20,17 +21,24 @@ func NewLongPollingConnection() *LongPollingConnection {
 	return &LongPollingConnection{make(chan []protocol.Message, 1), false, ""}
 }
 
+func (lp *LongPollingConnection) enqueueMessages(msgs []protocol.Message) error {
+	select {
+	case lp.responseChan <- msgs:
+		return nil
+	default:
+		return errors.New("Response channel is full")
+	}
+}
+
 func (lp *LongPollingConnection) Send(msgs []protocol.Message) error {
-	lp.Closed = true
-	lp.responseChan <- msgs
-	return nil
+	lp.Close()
+	return lp.enqueueMessages(msgs)
 }
 
 func (lp *LongPollingConnection) SendJsonp(msgs []protocol.Message, jsonp string) error {
-	lp.Closed = true
+	lp.Close()
 	lp.jsonp = jsonp
-	lp.responseChan <- msgs
-	return nil
+	return lp.enqueueMessages(msgs)
 }
 
 func (lp *LongPollingConnection) IsConnected() bool {
@@ -51,26 +59,29 @@ func (lp LongPollingConnection) IsSingleShot() bool {
 
 func MakeLongPoll(msgs interface{}, server Server, w http.ResponseWriter) {
 	conn := NewLongPollingConnection()
+	done := make(chan bool, 1)
 	go func() {
 		server.HandleRequest(msgs, conn)
+		done <- true
 	}()
 
-	responseMsgs := <-conn.responseChan
-	bs, err := json.Marshal(responseMsgs)
-	if err != nil {
-		server.Logger().Warnf("While encoding response msgs: %s", err)
-	}
-
-	if conn.jsonp != "" {
-		jsonp := fmt.Sprintf("/**/%v(%v)", conn.jsonp, string(bs))
-		bs = []byte(jsonp)
-		w.Header().Add("Content-Type", "text/javascript")
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-	}
-
-	_, err = w.Write(bs)
-	if err != nil {
-		server.Logger().Warnf("While writing HTTP response: %s", err)
+	select {
+	case responseMsgs := <-conn.responseChan:
+		if bs, err := json.Marshal(responseMsgs); err != nil {
+			server.Logger().Warnf("While encoding response msgs: %s", err)
+		} else {
+			if conn.jsonp != "" {
+				jsonp := fmt.Sprintf("/**/%v(%v)", conn.jsonp, string(bs))
+				bs = []byte(jsonp)
+				w.Header().Add("Content-Type", "text/javascript")
+			} else {
+				w.Header().Add("Content-Type", "application/json")
+			}
+			if _, err := w.Write(bs); err != nil {
+				server.Logger().Warnf("While writing HTTP response: %s", err)
+			}
+		}
+	case <-done:
+		server.Logger().Debugf("No response")
 	}
 }
