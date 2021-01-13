e74a9b979bbbda2567efdf1a6e4f0cea8ccc6104
Denis Sablic <denis.sablic@gmail.com>
2021-01-13 09:43:04 +0100

    Refactor/simplify subscription register

--- memory/subscription_register.go
+++ memory/subscription_register.go
@@ -3,13 +3,16 @@ package memory
 import (
 	"sync"
 
-	"github.com/dsablic/faye-go/utils"
 	"github.com/uber-go/atomic"
 )
 
+type void struct{}
+type interfaceMap map[interface{}]void
+type stringMap map[string]void
+
 type SubscriptionRegister struct {
-	subscriberByPattern       map[string]*utils.ValueSet
-	patternsBySubscriber      map[interface{}]*utils.StringSet
+	subscriberByPattern       map[string]interfaceMap
+	patternsBySubscriber      map[interface{}]stringMap
 	SubscriberByPatternCount  *atomic.Uint64
 	PatternsBySubscriberCount *atomic.Uint64
 	mutex                     sync.RWMutex
@@ -17,9 +20,9 @@ type SubscriptionRegister struct {
 
 func NewSubscriptionRegister() *SubscriptionRegister {
 	return &SubscriptionRegister{
-		subscriberByPattern:       make(map[string]*utils.ValueSet),
+		subscriberByPattern:       make(map[string]interfaceMap),
 		SubscriberByPatternCount:  atomic.NewUint64(0),
-		patternsBySubscriber:      make(map[interface{}]*utils.StringSet),
+		patternsBySubscriber:      make(map[interface{}]stringMap),
 		PatternsBySubscriberCount: atomic.NewUint64(0),
 	}
 }
@@ -33,18 +36,18 @@ func (sr *SubscriptionRegister) AddSubscription(subscriber interface{}, patterns
 	sr.mutex.Lock()
 	defer sr.mutex.Unlock()
 
+	if _, ok := sr.patternsBySubscriber[subscriber]; !ok {
+		sr.patternsBySubscriber[subscriber] = make(stringMap)
+	}
+
 	for _, pattern := range patterns {
 		if _, ok := sr.subscriberByPattern[pattern]; !ok {
-			sr.subscriberByPattern[pattern] = utils.NewValueSet()
+			sr.subscriberByPattern[pattern] = make(interfaceMap)
 		}
-		sr.subscriberByPattern[pattern].Add(subscriber)
+		sr.subscriberByPattern[pattern][subscriber] = void{}
+		sr.patternsBySubscriber[subscriber][pattern] = void{}
 	}
 
-	if _, ok := sr.patternsBySubscriber[subscriber]; !ok {
-		sr.patternsBySubscriber[subscriber] = utils.NewStringSet()
-	}
-
-	sr.patternsBySubscriber[subscriber].AddMany(patterns)
 	sr.updateCounts()
 }
 
@@ -54,37 +57,39 @@ func (sr *SubscriptionRegister) RemoveSubscription(subscriber interface{}, patte
 
 	for _, pattern := range patterns {
 		if p, ok := sr.subscriberByPattern[pattern]; ok {
-			p.Remove(subscriber)
+			delete(p, subscriber)
 		}
 		if s, ok := sr.patternsBySubscriber[subscriber]; ok {
-			s.Remove(pattern)
+			delete(s, pattern)
 		}
 	}
 	sr.updateCounts()
 }
 
 func (sr *SubscriptionRegister) GetSubscribers(patterns []string) []interface{} {
-	set := utils.NewValueSet()
+	arr := make([]interface{}, 0)
 	sr.mutex.RLock()
 	defer sr.mutex.RUnlock()
 
 	for _, pattern := range patterns {
-		if p, ok := sr.subscriberByPattern[pattern]; ok {
-			set.AddMany(p.GetAll())
+		if subscribers, ok := sr.subscriberByPattern[pattern]; ok {
+			for subscriber := range subscribers {
+				arr = append(arr, subscriber)
+			}
 		}
 	}
-	return set.GetAll()
+	return arr
 }
 
 func (sr *SubscriptionRegister) RemoveClient(subscriber interface{}) {
 	sr.mutex.Lock()
 	defer sr.mutex.Unlock()
 
-	if p, ok := sr.patternsBySubscriber[subscriber]; ok {
-		for _, pattern := range p.GetAll() {
-			if s, ok := sr.subscriberByPattern[pattern]; ok {
-				s.Remove(subscriber)
-				if s.Length() == 0 {
+	if patterns, ok := sr.patternsBySubscriber[subscriber]; ok {
+		for pattern := range patterns {
+			if subscribers, ok := sr.subscriberByPattern[pattern]; ok {
+				delete(subscribers, subscriber)
+				if len(subscribers) == 0 {
 					delete(sr.subscriberByPattern, pattern)
 				}
 			}
